73d689e37c71e6192b302ea64d74dad5bb1aded1
Joshua Gardiner <joshua.gardiner268@gmail.com>
2023-10-17 22:13:15 -0400

    Completed Exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,28 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
     const squares = document.querySelectorAll('#board div');
+    const status = document.getElementById('status');
     let currentPlayer = 'X';
+    let moves = 0;  // To keep track of the number of moves made.
 
-    squares.forEach(function(square, index) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    squares.forEach(square => {
         // Add the basic 'square' class to style each square
         square.classList.add('square');
 
         // Click event listener to add X or O
         square.addEventListener('click', function() {
-            // If the square is empty, put either X or O
-            if (square.textContent === '') {
+            if (!square.textContent) {
                 square.textContent = currentPlayer;
                 square.classList.add(currentPlayer);
+                moves++;
 
-                // Switch to the other player for the next turn
-                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+                if (checkWinner(currentPlayer)) {
+                    status.textContent = `Congratulations! ${currentPlayer} is the Winner!`;
+                    status.classList.add('you-won');
+                } else if (moves === 9) {
+                    status.textContent = "It's a Tie!";
+                } else {
+                    // Switch to the other player for the next turn
+                    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+                }
             }
         });
 
         // Mouseover event listener to show the hover effect
         square.addEventListener('mouseover', function() {
-            if (square.textContent === '') {
-                square.classList.add('hover');
-            }
+            square.classList.add('hover');
         });
 
         // Mouseout event listener to remove the hover effect
@@ -30,4 +48,10 @@ document.addEventListener('DOMContentLoaded', function() {
             square.classList.remove('hover');
         });
     });
+
+    function checkWinner(player) {
+        return winningCombinations.some(combination => {
+            return combination.every(index => squares[index].textContent === player);
+        });
+    }
 });
