d6af82ff0b67801bb804c8f0f9db568611ee15fe
Joshua Gardiner <joshua.gardiner268@gmail.com>
2023-10-17 21:56:59 -0400

    Completed Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,8 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
     // Select all the divs inside the game board (with the ID "board")
     let squares = document.querySelectorAll('#board > div');
     
-    // Loop through each square and add the 'square' class
-    squares.forEach(function(square) {
+    // Define a variable to track the current player. Start with 'X'
+    let currentPlayer = 'X';
+
+    // Define an empty array to keep track of the game state
+    let gameState = ['', '', '', '', '', '', '', '', ''];
+
+    // Loop through each square
+    squares.forEach(function(square, index) {
         square.classList.add('square');
+
+        // Add a click event listener to each square
+        square.addEventListener('click', function() {
+            // Check if the square is already occupied. If not, then proceed
+            if (square.textContent === '') {
+                // Set the content of the square to the current player (either 'X' or 'O')
+                square.textContent = currentPlayer;
+
+                // Add the class 'X' or 'O' for styling
+                square.classList.add(currentPlayer);
+
+                // Update the game state
+                gameState[index] = currentPlayer;
+
+                // Switch the current player for the next turn
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+        });
     });
 });
